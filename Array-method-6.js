d8bc1c0f69bf1b173664defa06afd85b4e16e467
Arunkum20072002 <arunkumarmarikani10@gmail.com>
2023-12-04 11:22:11 +0530

    Array-method-6

---
+++ Array-method-6.js
@@ -0,0 +1,8 @@
+document.write("6. Add Lemon and Pineapple in the frond of given array"+"<br>")
+document.write("-------------------------------------------------------"+"<br>")
+
+var fruits = ["Banana","Orange","Apple","Mango"]; 
+document.write(fruits+"<br>")
+
+fruits.unshift("Lemon","Pineapple")
+document.write(fruits+"<br>")
